87261e667ca372e8724856a9f2f5d7dbf0a3a36c
Anny27-pixel <jabintasmin102523@gmail.com>
2022-09-14 23:43:05 +0600

    float seconds convert into int seconds

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ const start = () => {
 };
 
 // START Countdown
-addEventListener("click", start());
+addEventListener("click", start);
 
 // If history exists, show it
 displayHistory();
@@ -132,7 +132,7 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpent = parseInt((currentTime - startTime) / 1000);
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
